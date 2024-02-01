f4af5d91933432ff567da35f662138cce75a974a
krasutskaaa <krasutskadiana@gmail.com>
2024-02-01 20:16:54 +0200

    The interface for Card and Client classes was added

---
+++ Abstractions/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Abstractions
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public DateTime CreateTime { get; init; } = DateTime.Now;
+        public DateTime UpdateTime { get; set; } = DateTime.Now;
+    }
+}
