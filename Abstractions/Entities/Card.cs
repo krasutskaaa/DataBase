c74e470271edd8cedde0520e5f383bc9f53dbedd
krasutskaaa <krasutskadiana@gmail.com>
2024-02-01 20:19:14 +0200

    The Card class was changed according to Entity interface and some fiels were changed

---
+++ Abstractions/Entities/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Abstractions.Entities
+{
+    public class Card: Entity
+    {
+        public Guid ClientId { get; init; }
+        public Type CardType { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
+
+public enum Type
+{
+    Standart,
+    Premium
+}
