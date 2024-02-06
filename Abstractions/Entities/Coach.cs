96a7ec290713fec46d9297e15845ccbb7770e9a1
krasutskaaa <krasutskadiana@gmail.com>
2024-02-06 23:18:56 +0200

    the Coach class was added as a new entity

---
+++ Abstractions/Entities/Coach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Abstractions.Entities
+{
+    public class Coach: Entity, AliveEntity
+    {
+        public string RealName { get; set; }
+        public string Username { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
