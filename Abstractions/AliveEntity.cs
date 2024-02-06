e046c64e4a1b9a1ca55432971b90c1555df9ca28
krasutskaaa <krasutskadiana@gmail.com>
2024-02-06 23:13:22 +0200

    new interface for alive entities was added

---
+++ Abstractions/AliveEntity.cs
@@ -0,0 +1,30 @@
+﻿using DataBase.Abstractions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Abstractions
+{
+    public interface AliveEntity
+    {
+        public string RealName { get; set; }
+        public string Username { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Bigender,
+        Transgender,
+        Gey,
+        Lesbian,
+        Bisexual,
+        Demigirl,
+        Demiboy,
+        Asexual
+    }
+}
