af4fb2117962fcd28024ea21fcd397520a900876
krasutskaaa <krasutskadiana@gmail.com>
2024-02-08 18:47:27 +0200

    a new property was added: Guid CoachId can be null

--- Abstractions/Entities/Client.cs
+++ Abstractions/Entities/Client.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataBase.Abstractions.Entities
 {
-    public class Client: Entity, AliveEntity
+    public class Client : Entity, AliveEntity
     {
         public string RealName { get; set; }
         public string Username { get; set; }
         public Gender Gender { get; set; }
-        public Guid CoachId { get; init; }
+        public Guid? CoachId { get; init; }
     }
 
    
